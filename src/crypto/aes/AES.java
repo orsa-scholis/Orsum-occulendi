bc3dbdfaeeb9f09462e955ac77e9ff6c3904c117
Philipp Fehr <calvin.philipp@gmail.com>
2017-03-16 16:02:58 +0100

    End Class Upload

--- src/crypto/aes/AES.java
+++ src/crypto/aes/AES.java
@@ -5,19 +5,37 @@ public class AES {
 	public byte[] output;
 	public byte[] key;
 	public AESError error;
-	
+
 	public AES(byte[] input, byte[] key) {
 		this.input = input;
 		this.key = key;
 		this.error = AESError.noErr;
 	}
-	
+
 	public void encrypt() {
 		this.output = input; // temporary!!!
 	}
-	
+
 	public void decrypt() {
 		this.output = input; // temp
+	}
+
+	private byte[][] addRoundKey(byte[][] state, byte[][] roundKey, boolean inverse) throws Exception
+	{
+		if ( state.length != roundKey.length || state[0].length != roundKey[0].length ){
+			this.error = AESError.unequalLengthError;
+			throw new Exception();
+		}
+
+		byte[][] tmp = new byte[][]{};
+		for ( int i = 0; i < state.length; i++ )
+		{
+			for ( int j = 0; j < state.length; j++ )
+			{
+				tmp[i][j] = (byte) (state[i][j] ^ roundKey[i][j]);
+			}
+		}
+		return tmp;
 	}
 
 	public byte[] getInput() {
--- src/crypto/cli/CryptoTestingCLI.java
+++ src/crypto/cli/CryptoTestingCLI.java
@@ -9,34 +9,34 @@ import crypto.aes.AES;
 public class CryptoTestingCLI {
 	public void main() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		while (true) {
 			System.out.println("Encrypt (e) or decrypt (d) or quit (q)?");
 			String method = br.readLine().toLowerCase();
-			
+
 			if (!method.equals("e") && !method.equals("d") && !method.equals("q")) {
 				System.out.println("Unknown method!");
 				continue;
 			} else if (method.equals("q")) {
 				break;
 			}
-			
+
 			System.out.println("Enter text to en/decrypt: ");
 			String plaintext = br.readLine();
-			
+
 			System.out.println("Enter key: ");
 			String key = br.readLine();
-			
-			AES aes = new AES(plaintext, key.getBytes());
-			
+
+			AES aes = new AES(plaintext.getBytes(), key.getBytes());
+
 			if (method.equals("e")) {
 				aes.encrypt();
 			} else {
 				aes.decrypt();
 			}
-			
+
 			System.out.println("Output: ");
-			System.out.println(aes.output);
+			System.out.println(new String(aes.output));
 			System.out.println("----------");
 		}
 	}
